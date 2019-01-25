d34132f87e85b0f854e063927d88121a1413e98e
LalitaS <LalitaS@MAQ40923>
2019-01-25 17:51:36 +0530

    Added header data bag

--- src/CustomVisuals/Published/DynamicTooltip/src/visual.ts
+++ src/CustomVisuals/Published/DynamicTooltip/src/visual.ts
@@ -74,7 +74,7 @@ module powerbi.extensibility.visual {
         displayUnits: number;
     }
 
-    export let visualProperties : {
+    export let visualProperties: {
         Itooltip: {
             text: DataViewObjectPropertyIdentifier;
             header: DataViewObjectPropertyIdentifier;
@@ -86,7 +86,7 @@ module powerbi.extensibility.visual {
             displayUnits: DataViewObjectPropertyIdentifier;
         };
     }
-     = {
+        = {
         Itooltip: {
             text: <DataViewObjectPropertyIdentifier>{ objectName: 'tooltip', propertyName: 'text' },
             header: <DataViewObjectPropertyIdentifier>{ objectName: 'tooltip', propertyName: 'header' },
@@ -108,13 +108,16 @@ module powerbi.extensibility.visual {
         private root: d3.Selection<SVGElement>;
         private image: d3.Selection<SVGElement>;
         private tooltipText: string;
+        private headerText: string;
         private header: string;
         private opacity: number;
-        private dataViews : DataView;
+        private dataViews: DataView;
         private imageurl: string;
         private showMeasure: boolean;
         private textPrecision: number;
         private displayUnits: number;
+        private headerIndex: number;
+        private measureIndex: number;
 
         constructor(options: VisualConstructorOptions) {
             this.host = options.host;
@@ -127,14 +130,11 @@ module powerbi.extensibility.visual {
                 .append('img').classed('dynamicTooltip_img', true)
                 .attr('src', this.imageurl);
             this.tooltipServiceWrapper = createTooltipServiceWrapper(this.host.tooltipService, options.element);
-            this.tooltipText = 'Tooltip text here';
-            this.header = 'Tooltip header text here';
             this.showMeasure = false;
             this.textPrecision = 0;
             this.displayUnits = 0;
         }
-
-        public update(options: VisualUpdateOptions) : void {
+        public update(options: VisualUpdateOptions): void {
 
             let textSetting: Itooltip;
             textSetting = this.getDefaultTextSettings();
@@ -171,66 +171,179 @@ module powerbi.extensibility.visual {
             let dataViews: DataView[];
             dataViews = options.dataViews;
             if (dataViews && dataViews[0] && dataViews[0].metadata) {
-
                 let dataView: DataView;
                 dataView = this.dataViews = options.dataViews[0];
                 let imagePatt: RegExp;
                 imagePatt = new RegExp('^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png|svg)$');
-                let selectedImage: string;
                 let validImage: boolean;
                 let tooltipSettings: Itooltip;
                 tooltipSettings = this.getToolTipSettings(dataView);
                 let measureSettings: ImeasureSettings;
                 measureSettings = this.getMeasureSettings(dataView);
-
-                this.tooltipText = tooltipSettings.text;
-                this.header = tooltipSettings.header;
+                const values: DataViewValueColumns = options.dataViews[0].categorical.values;
+                const iTotalColumns: number = values.length;
+                let headerIndex: number;
+                let measureIndex: number;
+                this.index(iTotalColumns, values);
+                headerIndex = this.headerIndex;
+                measureIndex = this.measureIndex;
                 if (measureSettings.show) {
-                    if (dataViews[0].categorical && dataViews[0].categorical.values && dataViews[0].categorical.values[0]) {
-                        this.header = dataViews[0].categorical.values[0].source.displayName;
-                        this.tooltipText = dataViews[0].categorical.values[0].values
-                            .toString() !== '' ? dataViews[0].categorical.values[0].values.toString() : tooltipSettings.text;
-                        let decimalPlaces: number;
+                    if (dataViews[0].categorical && values && headerIndex > -1 && measureIndex > -1) {
+                        this.headerData(values[headerIndex], tooltipSettings);
+                        this.measuredata(values[measureIndex], tooltipSettings);
+                    } else
+                        if (values[0].source.roles.header) {
+                            this.headerData(values[headerIndex], tooltipSettings);
+                            this.tooltipText = '';
+                        } else
+                            if (values[0].source.roles.measure) {
+                                this.measuredata(values[measureIndex], tooltipSettings);
+                                this.headerText = ' ';
 
-                        decimalPlaces = measureSettings.textPrecision;
-
-                        let displayUnits: number;
-
-                        displayUnits = measureSettings.displayUnits;
-
-                        let formatter: utils.formatting.IValueFormatter;
-                        if ((String((dataViews[0].categorical.values[0].source.format)) === 'undefined') ||
-                         (String((dataViews[0].categorical.values[0].source.format)) === 'dd MMMM yyyy') ) {
-                            formatter = valueFormatter.create({
-                                format: dataViews[0].categorical.values[0].source.format
-                            });
-                         } else {
-                        formatter = valueFormatter.create({
-                            format: dataViews[0].categorical.values[0].source.format,
-                            value: displayUnits, precision: decimalPlaces
-                        });
-                    }
-                        let formattedData: string;
-                        formattedData = formatter.format(this.tooltipText);
-                        this.tooltipText = formattedData;
-
-                    }
+                            }
+                } else {
+                    this.tooltipText = tooltipSettings.text;
+                    this.headerText = tooltipSettings.header;
                 }
-                selectedImage = tooltipSettings.imageurl;
-                validImage = imagePatt.test(selectedImage);
-                this.image = validImage ? img.attr('src', selectedImage) : img.attr('src', textSetting.imageurl);
+                validImage = imagePatt.test(tooltipSettings.imageurl);
+                this.image = validImage ? img.attr('src', tooltipSettings.imageurl) : img.attr('src', textSetting.imageurl);
                 let objects: DataViewObjects;
                 objects = dataViews[0].metadata.objects;
             }
-
-            if (this.tooltipText !== '') {
+            if (this.tooltipText !== '' || this.header !== '' || this.headerText !== '') {
                 this.tooltipServiceWrapper.addTooltip(this.root,
                                                       (tooltipEvent: TooltipEventArgs<number>) =>
-                        this.getTooltipData(this.tooltipText, this.header),
+                        this.getTooltipData(this.tooltipText, this.header, this.headerText),
                                                       (tooltipEvent: TooltipEventArgs<number>) => null);
             }
         }
+        public index(nColumns: number, values: DataViewValueColumns): void {
+            const iTotalColumns: number = values.length;
+            this.headerIndex = -1;
+            this.measureIndex = -1;
+            for (let iCount: number = 0; iCount < nColumns; iCount++) {
+                if (values[iCount].source.roles.header) {
+                    this.headerIndex = iCount;
+                }
+                if (values[iCount].source.roles.measure) {
+                    this.measureIndex = iCount;
+                }
+            }
+        }
+        public headerData(values: DataViewValueColumn, tooltipSettings: Itooltip): void {
+            let measureSettings: ImeasureSettings;
+            measureSettings = this.getMeasureSettings(this.dataViews);
+            this.headerText = values.values
+                .toString() !== '' ? values.values.toString() : tooltipSettings.text;
+            let decimalPlaces: number;
 
+            decimalPlaces = measureSettings.textPrecision;
+
+            let displayUnits: number;
+
+            displayUnits = measureSettings.displayUnits;
+            let formatter: utils.formatting.IValueFormatter;
+            const number: PrimitiveValue = values.values[0];
+            if (String((values.source.format)) === 'undefined' && typeof (number) === 'number') {
+                values.source.format = valueFormatter.DefaultNumericFormat;
+            }
+
+            if ((String((values.source.format)) === 'undefined') ||
+                (String((values.source.format)) === 'dd MMMM yyyy')) {
+                formatter = valueFormatter.create({
+                    format: values.source.format
+                });
+                let formatData: string;
+                formatData = formatter.format(this.headerText);
+                this.headerText = formatData;
+
+            } else {
+                let primaryFormatterVal: number = 0;
+                // For display units auto
+                if (displayUnits === 0) {
+                    const alternateFormatter: number = parseInt(this.headerText.toString(), 10).toString().length;
+                    if (alternateFormatter > 9) {
+                        primaryFormatterVal = 1e9;
+                    } else if (alternateFormatter <= 9 && alternateFormatter > 6) {
+                        primaryFormatterVal = 1e6;
+                    } else if (alternateFormatter <= 6 && alternateFormatter > 4) {
+                        primaryFormatterVal = 1e3;
+                    } else {
+                        primaryFormatterVal = 10;
+                    }
+                } else {
+                    primaryFormatterVal = displayUnits;
+                }
+                formatter = valueFormatter.create({
+                    format: values.source.format ? values.source.format : valueFormatter.DefaultNumericFormat,
+                    value: primaryFormatterVal,
+                    precision: decimalPlaces
+                });
+                let formattedData: string;
+                if (this.headerText !== tooltipSettings.text) {
+                    // Converted to number for display units none
+                    formattedData = formatter.format(Number(this.headerText));
+                    this.headerText = formattedData;
+                }
+            }
+        }
+        public measuredata(values: DataViewValueColumn, tooltipSettings: Itooltip): void {
+            let measureSettings: ImeasureSettings;
+            measureSettings = this.getMeasureSettings(this.dataViews);
+            this.tooltipText = values.values
+                .toString() !== '' ? values.values.toString() : tooltipSettings.text;
+            let decimalPlaces: number;
+
+            decimalPlaces = measureSettings.textPrecision;
+
+            let displayUnits: number;
+
+            displayUnits = measureSettings.displayUnits;
+            let formatter: utils.formatting.IValueFormatter;
+            const number: PrimitiveValue = values.values[0];
+            if (String((values.source.format)) === 'undefined' && typeof (number) === 'number') {
+                values.source.format = valueFormatter.DefaultNumericFormat;
+            }
+
+            if ((String((values.source.format)) === 'undefined') ||
+                (String((values.source.format)) === 'dd MMMM yyyy')) {
+                formatter = valueFormatter.create({
+                    format: values.source.format
+                });
+                let formatData: string;
+                formatData = formatter.format(this.tooltipText);
+                this.tooltipText = formatData;
+
+            } else {
+                let primaryFormatterVal: number = 0;
+                // For display units auto
+                if (displayUnits === 0) {
+                    const alternateFormatter: number = parseInt(this.tooltipText.toString(), 10).toString().length;
+                    if (alternateFormatter > 9) {
+                        primaryFormatterVal = 1e9;
+                    } else if (alternateFormatter <= 9 && alternateFormatter > 6) {
+                        primaryFormatterVal = 1e6;
+                    } else if (alternateFormatter <= 6 && alternateFormatter > 4) {
+                        primaryFormatterVal = 1e3;
+                    } else {
+                        primaryFormatterVal = 10;
+                    }
+                } else {
+                    primaryFormatterVal = displayUnits;
+                }
+                formatter = valueFormatter.create({
+                    format: values.source.format ? values.source.format : valueFormatter.DefaultNumericFormat,
+                    value: primaryFormatterVal,
+                    precision: decimalPlaces
+                });
+                let formattedData: string;
+                if (this.tooltipText !== tooltipSettings.text) {
+                    // Converted to number for display units none
+                    formattedData = formatter.format(Number(this.tooltipText));
+                    this.tooltipText = formattedData;
+                }
+            }
+        }
         public getDefaultTextSettings(): Itooltip {
             return {
                 text: 'sample',
@@ -349,11 +462,15 @@ module powerbi.extensibility.visual {
             return objectEnumeration;
         }
 
-        private getTooltipData(tooltip: string, header: string): VisualTooltipDataItem[] {
+        private getTooltipData(tooltip: string, header: string, headerText: string): VisualTooltipDataItem[] {
+            if (tooltip === '' && header === '') {
+                return null;
+            }
+
             return [{
                 displayName: '',
                 value: tooltip,
-                header: header
+                header: headerText
             }];
         }
 
